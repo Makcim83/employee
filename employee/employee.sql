fecfa8a5239274de762fb5084aec7143dd0a65cd
admin <mmd83@63.ru>
2023-03-17 23:23:56 +0400

    employee.sql

---
+++ employee/employee.sql
@@ -0,0 +1,8 @@
+CREATE TABLE employee
+(
+    id         BIGSERIAL   NOT NULL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name  VARCHAR(50) NOT NULL,
+    gender     VARCHAR(6)  NOT NULL,
+    age        INT         NOT NULL
+);
